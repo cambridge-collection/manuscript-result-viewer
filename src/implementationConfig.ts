9ab9aa40b56fdcd0740b355ff9145b19e67f12dc
Michael Hawkins <mjh39@cam.ac.uk>
2026-04-23 16:11:13 +0100

    fix(facets): remove date_cert, origin and music

--- src/implementationConfig.ts
+++ src/implementationConfig.ts
@@ -4,7 +4,7 @@
 const api_url: string = "https://search.medieval.lib.cam.ac.uk"
 
 // Specify facets to be shown in the sidebar, displayed in array order.
-const desired_facets: string[] = [ 'lang_sm', 'ms_date_sm', 'ms_datecert_s', 'ms_origin_sm', 'author_sm', 'ms_materials_sm', 'ms_decotype_sm', 'ms_music_b', 'ms_bindingdate_sm', 'ms_digitized_s', 'ms_repository_s', 'ms_collection_s' ]
+const desired_facets: string[] = [ 'lang_sm', 'ms_date_sm', 'author_sm', 'ms_materials_sm', 'ms_decotype_sm', 'ms_bindingdate_sm', 'ms_digitized_s', 'ms_repository_s', 'ms_collection_s' ]
 
 // A translation table to generate the nice title/fieldnames. I don't believe count is being used.
 const facet_key: Record<string, { name: string; count: number }> = {
@@ -12,12 +12,9 @@ const facet_key: Record<string, { name: string; count: number }> = {
   'editor_sm': { name: 'Editor', count: 5 },
   'lang_sm': { name: 'Language', count: 5 },
   'ms_date_sm': { name: 'Century', count: 5 },
-  'ms_datecert_s': { name: 'Date Certainty', count: 5 },
-  'ms_origin_sm': { name: 'Origin', count: 5 },
   'wk_subjects_sm': { name: 'Subjects', count: 999 },
   'ms_materials_sm': { name: 'Materials', count: 5 },
   'ms_decotype_sm': { name: 'Decoration', count: 5 },
-  'ms_music_b': { name: 'Musical Notation', count: 5 },
   'ms_bindingdate_sm': { name: 'Binding Century', count: 5 },
   'ms_digitized_s': { name: 'Digital Facsimile Online', count: 5 },
   'ms_repository_s': { name: 'Repository', count: 5 },
